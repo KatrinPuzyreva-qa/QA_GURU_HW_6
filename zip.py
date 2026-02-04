5594651c53a45667c16d4caed170bc820d8fdd83
Екатерина <131952002+KatrinPuzyreva-qa@users.noreply.github.com>
2026-02-04 18:34:32 +0400

    fix

--- zip.py
+++ zip.py
@@ -1,56 +1,55 @@
-import os
 import zipfile
-import pandas as pd
-from PyPDF2 import PdfReader
-import warnings
+import os
+import pytest
+from openpyxl.reader.excel import load_workbook
+from pypdf import PdfReader
+from io import BytesIO
 
-warnings.filterwarnings("ignore", category=UserWarning, module='openpyxl.styles.stylesheet')
+files_path = os.path.join(os.getcwd(), "files")
+pdf_path = os.path.join(files_path, "report.pdf")
+xlsx_path = os.path.join(files_path, "B-4.xlsx")
+csv_path = os.path.join(files_path, "QUOTA_FILE.csv")
+zip_path = os.path.join(files_path, "zipfile.zip")
+
+# Объявляем фикстуру
+@pytest.fixture(scope="session")
+def prepare_zip_archive():
+    with zipfile.ZipFile(zip_path, 'w') as zf:
+        for file in (pdf_path, xlsx_path, csv_path):
+            zf.write(file, os.path.basename(file))
+    yield
+    os.remove(zip_path)
+
+def test_check_pdf_file(prepare_zip_archive):
+    with zipfile.ZipFile(zip_path, "r") as zp:
+        with zp.open("report.pdf") as file:
+            pdf_data = file.read()
+            reader = PdfReader(BytesIO(pdf_data))
+
+            # Проверка количества страниц
+            number_of_pages = len(reader.pages)
+            assert number_of_pages == 1, 'Ошибка в количестве страниц в файле Pdf'
+
+            # Проверка содержания текста
+            page = reader.pages[0]
+            text = page.extract_text()
+            assert """Пример документа в формате PDF""" in text, 'Ошибка в проверке содержимого PDF'
 
 
-def create_archive():
-    # Проверяем существует ли папка
-    if not os.path.exists(r'C:\\Users\\sv_pa\\PycharmProjects\\QA_GURU_HW_6\\tmp'):
-        # Создаем папку если её нет
-        os.mkdir(r'C:\\Users\\sv_pa\\PycharmProjects\\QA_GURU_HW_6\\tmp')
+def test_check_csv_file(prepare_zip_archive):
+    with zipfile.ZipFile(zip_path, "r") as zp:
+        with zp.open("QUOTA_FILE.csv") as file:
+            text = file.readline().decode('utf-8')
+            assert "quotaCode" in text, 'Ошибка в тексте файла csv'
 
-    # Создаем архив
-    with zipfile.ZipFile(r'C:\\Users\\sv_pa\\PycharmProjects\\QA_GURU_HW_6\\tmp\\archive.zip', 'w') as zf:
-        # Добавляем файлы в архив
-        for file in ['B-4.xlsx', 'NEW_FILE.csv', '']:
-            # Склеиваем путь к файлам которые добавляют в архив
-            add_file = os.path.join(r'C:\\Users\\sv_pa\\PycharmProjects\\QA_GURU_HW_6', file)
-            # Добавляем файл в архив
-            zf.write(add_file, os.path.basename(add_file))
 
-# Проверяем содержимое файлов внутри архива без распаковки
-def check_files_inside_zip(zip_path):
-    with zipfile.ZipFile(zip_path, 'r') as zf:
-        for filename in zf.namelist():
-            print(f"\\nChecking {filename}:")
+def test_check_xlsx_file(prepare_zip_archive):
+    with (zipfile.ZipFile(zip_path, "r") as zp):
+        with zp.open("B-4.xlsx") as file:
+            text = load_workbook(file)
+            sheet = text.active
+            assert sheet.cell(row=7, column=2).value == 'только для зарегистрированных пользователей'
 
-            if filename.endswith('.pdf'):
-                # Читаем метаданные PDF
-                with zf.open(filename, 'r') as f:  # Убедитесь, что файл открывается в двоичном режиме
-                    reader = PdfReader(f)
-                    info = reader.metadata
-                    print(info.get('/Title'))
-
-            elif filename.endswith('.xlsx'):
-                # Просматриваем лист Excel
-                with zf.open(filename) as f:
-                    df = pd.read_excel(f, engine="openpyxl")
-                    print(df.head())
-
-            elif filename.endswith('.csv'):
-                # Просматриваем строки CSV
-                with zf.open(filename) as f:
-                    data = f.read().decode('utf-8')
-                    print(data.splitlines()[:5])
-
-# Создаем архив
-create_archive()
-
-# Проверяем содержимое архива
-check_files_inside_zip(r'C:\\Users\\sv_pa\\PycharmProjects\\QA_GURU_HW_6\\tmp\\archive.zip')
-
+            headers = [cell.value for cell in sheet[1]]
+            assert headers == ['Условия', 'Варианты условий', 1, 2, 3, 4, 5, 6, 7, 8, None],'Ошибка в заголовках столбцов'
 
